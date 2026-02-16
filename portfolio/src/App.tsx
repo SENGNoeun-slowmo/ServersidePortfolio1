978bad2d0434a6f06b56039f9bc380e456ebafae
SengNoeun <sengnoeun.biu@gmail.com>
2026-02-16 12:45:48 +0700

    fix loading1

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,7 +1,6 @@
 // src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Navbar';       // your fixed header
-import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
